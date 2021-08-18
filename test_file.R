f9d5540ed02f5b1d0686f1cde1143217fbb20e7e
Seth Berry <seth.berry@nd.edu>
2021-08-18 15:10:37 -0400

    Committing crimes against github!

--- test_file.R
+++ test_file.R
@@ -1,2 +1,16 @@
 x <- 1:10
 y <- 1:11
+
+library(ggplot2)
+
+my_plot <- ggplot(data = mtcars, 
+                  mapping = aes(x = disp, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = "lm") +
+  theme_minimal()
+
+plotly::ggplotly(my_plot)
+
+st_joe_land <- read.delim(file = "stJoeLand.csv", sep = ",")
+
+st_joe_land <- read.delim(file = "C:/Users/sberry5/Documents/msba_intro_repo/stJoeLand.csv", sep = ",")
